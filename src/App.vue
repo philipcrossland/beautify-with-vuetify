f8f57498a9a125456b6baa9bda06bcdbaaeaab50
Phil Crossland <phil@thirdwheelstudios.com>
2021-04-09 09:52:51 +0100

    End of L9 - Themes

--- src/App.vue
+++ src/App.vue
@@ -1,66 +1,72 @@
 <template>
-	<v-app>
-		<v-app-bar app color="primary" dark>
-			<v-toolbar-title>Vuetify Dashboard</v-toolbar-title>
-			<v-spacer></v-spacer>
-			<v-btn
-				v-for="link in links"
-				:key="`${link.label}-header-link`"
-				text
-				rounded
-				:to="link.url"
-			>
-				{{ link.label }}
-			</v-btn>
-		</v-app-bar>
-		<v-main>
-			<router-view></router-view>
-		</v-main>
-		<v-footer color="primary lighten-1" padless>
-			<v-layout justify-center wrap>
-				<v-btn
-					v-for="link in links"
-					:key="`${link.label}-footer-link`"
-					color="white"
-					text
-					rounded
-					class="my-2"
-					:to="link.url"
-				>
-					{{ link.label }}
-				</v-btn>
-				<v-flex primary lighten-2 py-4 text-center white--text xs12>
-					{{ new Date().getFullYear() }} — <strong>Vuetify Dashboard</strong>
-				</v-flex>
-			</v-layout>
-		</v-footer>
-	</v-app>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-toolbar-title>Vuetify Dashboard</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        v-for="link in links"
+        :key="`${link.label}-header-link`"
+        text
+        rounded
+        :to="link.url"
+      >
+        {{ link.label }}
+      </v-btn>
+      <v-btn text rounded @click="toggleTheme">Toggle Theme</v-btn>
+    </v-app-bar>
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+    <v-footer color="primary lighten-1" padless>
+      <v-layout justify-center wrap>
+        <v-btn
+          v-for="link in links"
+          :key="`${link.label}-footer-link`"
+          color="white"
+          text
+          rounded
+          class="my-2"
+          :to="link.url"
+        >
+          {{ link.label }}
+        </v-btn>
+        <v-flex primary lighten-2 py-4 text-center white--text xs12>
+          {{ new Date().getFullYear() }} — <strong>Vuetify Dashboard</strong>
+        </v-flex>
+      </v-layout>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
 export default {
-	name: 'App',
-	data() {
-		return {
-			links: [
-				{
-					label: 'Home',
-					url: '/'
-				},
-				{
-					label: 'Login',
-					url: '/login'
-				},
-				{
-					label: 'Signup',
-					url: '/signup'
-				},
-				{
-					label: 'Dashboard',
-					url: '/dashboard'
-				}
-			]
-		}
-	}
-}
+  name: "App",
+  data() {
+    return {
+      links: [
+        {
+          label: "Home",
+          url: "/",
+        },
+        {
+          label: "Login",
+          url: "/login",
+        },
+        {
+          label: "Signup",
+          url: "/signup",
+        },
+        {
+          label: "Dashboard",
+          url: "/dashboard",
+        },
+      ],
+    };
+  },
+  methods: {
+    toggleTheme() {
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+    },
+  },
+};
 </script>
